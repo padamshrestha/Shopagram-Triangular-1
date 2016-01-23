98b853f36c97c62a35e363e83397f6e7771e60e5
Chase Isley <Chase Isley>
2016-01-22 20:11:50 -0700

    Added SHopagram to Surface

--- src/app/shopagram/authentication/login/constants.js
+++ src/app/shopagram/authentication/login/constants.js
@@ -10,6 +10,7 @@
 
         .constant('API_ENDPOINT', {
           url: 'http://127.0.0.1:8080/api'
+        //   url: 'http://192.241.186.45:80/api'
           //  For a simulator use: url: 'http://127.0.0.1:8080/api'
         });
 })();
